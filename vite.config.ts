7682be50456a9f27b758f61eff9cc754c3f6c88e
YuliiaSydorenko <o972986990f@gmail.com>
2025-02-06 13:04:37 +0100

    2 commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'assets': path.resolve(__dirname, 'src/assets'),
+      'components': path.resolve(__dirname, 'src/components'),
+      'Homeworks': path.resolve(__dirname, 'src/homeworks'),
+      'Lessons': path.resolve(__dirname, 'src/Lessons'),
+      'pages': path.resolve(__dirname, 'src/pages'),
+      'consultations': path.resolve(__dirname, 'src/consultations'),
+      'styles': path.resolve(__dirname, 'src/styles')
+    }
+  },
   server: {
     open: true,
   },
